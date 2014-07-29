e719c615e5f7e610f8ef7e6facbb57fd00e914bf
Max Harlow <maxharlow@gmail.com>
2014-07-29 11:48:03 +0100

    Add loader

---
+++ Load.scala
@@ -0,0 +1,75 @@
+import java.io.File
+import org.anormcypher.{Cypher, Neo4jREST}
+
+object Load extends App {
+
+  println("""
+    ,---,
+    |___|    .        ___ __   _   _  ___  _  __
+    /``\ |---^-,_      |  |_) /_\ / `  |  / \ |_)
+   ( () )  ( o )||     |  | \ | | \_,  |  \_/ | \
+    \__/````\_/  \
+  """)
+
+  Neo4jREST.setServer("localhost")
+
+  load(new File("awards.csv"))
+
+  // TODO: deal with contracts awarded to multiple awardees (split on semicolon) -- will require Extract change (model contract as a node...)
+
+  def load(file: File) = {
+    val query = {
+      s"LOAD CSV WITH HEADERS FROM 'file://${file.getAbsolutePath}' AS line" +
+      """
+      FOREACH(companyNumber IN (CASE WHEN line.awardeeCompanyNumber <> '' THEN [line.awardeeCompanyNumber] ELSE [] END) |
+      MERGE (gd:GovDepartment {name: line.orgName}) ON CREATE SET
+        gd.orgContact = line.orgContact
+      MERGE (a:Organisation {companyNumber: companyNumber}) ON CREATE SET
+        a.name = line.awardeeName
+      CREATE (gd)-[:AWARDED_CONTRACT_TO {
+        noticeId: line.noticeId,
+        noticeType: line.noticeType,
+        noticeState: line.noticeState,
+        noticeStateChangeDate: toInt(replace(line.noticeStateChangeDate, '-', '')),
+        referenceNumber: line.referenceNumber,
+        awardDate: toInt(replace(line.awardDate, '-', '')),
+        publishedDate: toInt(replace(line.publishedDate, '-', '')),
+        value: toInt(line.value),
+        status: line.status,
+        title: line.title,
+        description: line.description,
+        region: line.region,
+        classification: line.classification,
+        numDocs: line.numDocs
+      }]->(a)
+      )
+      FOREACH(name IN (CASE WHEN line.awardeeCompanyNumber = '' THEN [line.awardeeName] ELSE [] END) |
+      MERGE (gd:GovDepartment {name: line.orgName}) ON CREATE SET
+        gd.orgContact = line.orgContact
+      MERGE (a:Organisation {name: name}) ON CREATE SET
+        a.companyNumber = line.awardeeCompanyNumber
+      CREATE (gd)-[:AWARDED_CONTRACT_TO {
+        noticeId: line.noticeId,
+        noticeType: line.noticeType,
+        noticeState: line.noticeState,
+        noticeStateChangeDate: toInt(replace(line.noticeStateChangeDate, '-', '')),
+        referenceNumber: line.referenceNumber,
+        awardDate: toInt(replace(line.awardDate, '-', '')),
+        publishedDate: toInt(replace(line.publishedDate, '-', '')),
+        value: toInt(line.value),
+        status: line.status,
+        title: line.title,
+        description: line.description,
+        region: line.region,
+        classification: line.classification,
+        numDocs: line.numDocs
+      }]->(a)
+      )
+      """
+    }
+    println(query)
+    val result = Cypher(query).execute()
+    if (!result) println(s" => failed to add ${file.getPath}")
+  }
+
+}
--- build.sbt
+++ build.sbt
@@ -1,6 +1,12 @@
 scalaVersion := "2.11.1"
 
+resolvers ++= Seq(
+  "Typesafe Releases" at "http://repo.typesafe.com/typesafe/releases",
+  "Anormcypher Repository" at "http://repo.anormcypher.org/"
+)
+
 libraryDependencies ++= Seq(
   "net.databinder.dispatch" %% "dispatch-core" % "0.11.1",
-  "com.github.tototoshi" %% "scala-csv" % "1.0.0"
+  "com.github.tototoshi" %% "scala-csv" % "1.0.0",
+  "org.anormcypher" %% "anormcypher" % "0.5.1"
 )
