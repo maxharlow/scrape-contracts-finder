9764041437762d50e33eb514fcb28a4162df8695
Max Harlow <maxharlow@gmail.com>
2014-09-27 22:35:53 +0100

    Tenders too

--- Extract.scala
+++ Extract.scala
@@ -23,6 +23,7 @@ object Extract extends App {
 
   def run() {
     val awardsCsv = CSVWriter.open(new File("awards.csv"))
+    val tendersCsv = CSVWriter.open(new File("tenders.csv"))
     val awardsHeaders = List(
       "noticeId",
       "noticePublishedDate",
@@ -45,17 +46,41 @@ object Extract extends App {
       "supplierName",
       "supplierCompanyNumber",
       "supplierContact")
+    val tendersHeaders = List(
+      "noticeId",
+      "noticePublishedDate",
+      "noticeType",
+      "noticeForm",
+      "noticeSystemState",
+      "noticeSystemStateChangeDate",
+      "noticeDocsNumber",
+      "contractTitle",
+      "contractDescription",
+      "contractLocation",
+      "contractClassifications",
+      "contractValueFrom",
+      "contractValueTo",
+      "buyerGroupId",
+      "buyerGroupName",
+      "buyerOrgName",
+      "buyerContact"
+    )
     awardsCsv writeRow awardsHeaders
+    tendersCsv writeRow tendersHeaders
     for {
       year <- 2011 to 2014
       month <- 1 to 12
     }
     yield for (data <- retrieve(year, month))
-    yield for (item <- process(data)) {
-      val awards = selectAwards(item)
-      awardsCsv writeRow awards.values.toSeq
+    yield for (notice <- process(data)) {
+      notice.label match {
+        case "CONTRACT_AWARD" => awardsCsv writeRow selectAward(notice).values.toSeq
+        case "CONTRACT" => tendersCsv writeRow selectTender(notice).values.toSeq
+        case "PRIOR_INFORMATION" => // ignore for now
+      }
     }
     awardsCsv.close()
+    tendersCsv.close()
   }
 
   def retrieve(year: Int, month: Int): Future[String] = {
@@ -71,14 +96,12 @@ object Extract extends App {
   def process(data: String): Seq[xml.Node] = {
     for {
       xml <- XML.loadString(data.dropWhile(_ != '<'))
-      body <- xml \ "NOTICES" \ "_"
-      awards <- body.filter(_.label == "CONTRACT_AWARD")
-      award <- awards
+      notices <- xml \ "NOTICES" \ "_"
     }
-    yield awards
+    yield notices
   }
 
-  def selectAwards(award: xml.Node): ListMap[String, String] = {
+  def selectAward(award: xml.Node): ListMap[String, String] = {
     ListMap(
       "noticeId" -> (award \ "SYSTEM" \ "NOTICE_ID").text,
       "noticePublishedDate" -> (award \ "SYSTEM" \ "SYSTEM_PUBLISHED_DATE").text,
@@ -113,6 +136,34 @@ object Extract extends App {
       "supplierName" -> (award \ "FD_CONTRACT_AWARD" \ "AWARD_OF_CONTRACT" \ "ECONOMIC_OPERATOR_NAME_ADDRESS" \ "CONTACT_DATA_WITHOUT_RESPONSIBLE_NAME" \ "ORGANISATION").map(_.text.trim).mkString(";"),
       "supplierCompanyNumber" -> (award \ "FD_CONTRACT_AWARD" \ "AWARD_OF_CONTRACT" \ "COMPANIES_HOUSE_URI_SUFFIX").map(_.text).mkString(";"),
       "supplierContact" -> (award \ "FD_CONTRACT_AWARD" \ "AWARD_OF_CONTRACT" \ "ECONOMIC_OPERATOR_NAME_ADDRESS" \ "EMAIL").map(_.text.trim).mkString(";")
+    )
+  }
+
+  def selectTender(tender: xml.Node): ListMap[String, String] = {
+    ListMap(
+      "noticeId" -> (tender \ "SYSTEM" \ "NOTICE_ID").text,
+      "noticePublishedDate" -> (tender \ "SYSTEM" \ "SYSTEM_PUBLISHED_DATE").text,
+      "noticeType" -> (tender \ "SYSTEM" \ "NOTICE_TYPE_FRIENDLY_NAME").text,
+      "noticeForm" -> {
+        val formNumber = (tender \ "@FORM").text.toInt
+        if (formNumber == 1 || formNumber == 2 || formNumber == 3) "Below-OJEU"
+        else if (formNumber == 90 || formNumber == 91) "Transparency"
+        else if (formNumber == 92 || formNumber == 93) "Subcontract" // never seems to occur?
+        else "UNKNOWN"
+      },
+      "noticeSystemState" -> (tender \ "SYSTEM" \ "SYSTEM_NOTICE_STATE").text,
+      "noticeSystemStateChangeDate" -> (tender \ "SYSTEM" \ "SYSTEM_NOTICE_STATE_CHANGE_DATE").text,
+      "noticeDocsNumber" -> (tender \ "FD_CONTRACT" \ "DOCUMENTS" \ "_").length.toString,
+      "contractTitle" -> (tender \ "FD_CONTRACT" \ "OBJECT_CONTRACT_INFORMATION" \ "DESCRIPTION_CONTRACT_INFORMATION" \ "TITLE_CONTRACT").text.trim,
+      "contractDescription" -> (tender \ "FD_CONTRACT" \ "OBJECT_CONTRACT_INFORMATION" \ "DESCRIPTION_CONTRACT_INFORMATION" \ "SHORT_CONTRACT_DESCRIPTION").text.trim,
+      "contractLocation" -> (tender \ "FD_CONTRACT" \ "OBJECT_CONTRACT_INFORMATION" \ "DESCRIPTION_CONTRACT_INFORMATION" \ "LOCATION_NUTS" \\ "p").head.text,
+      "contractClassifications" -> (tender \ "FD_CONTRACT" \ "OBJECT_CONTRACT_INFORMATION" \ "DESCRIPTION_CONTRACT_INFORMATION" \ "CPV" \\ "CPV_CODE").map(_.\("@CODE").text).mkString(";"),
+      "contractValueFrom" -> (tender \ "FD_CONTRACT" \ "OBJECT_CONTRACT_INFORMATION" \ "QUANTITY_SCOPE" \ "NATURE_QUANTITY_SCOPE" \ "COSTS_RANGE_AND_CURRENCY" \ "RANGE_VALUE_COST" \ "LOW_VALUE").text,
+      "contractValueTo" -> (tender \ "FD_CONTRACT" \ "OBJECT_CONTRACT_INFORMATION" \ "QUANTITY_SCOPE" \ "NATURE_QUANTITY_SCOPE" \ "COSTS_RANGE_AND_CURRENCY" \ "RANGE_VALUE_COST" \ "HIGH_VALUE").text,
+      "buyerGroupId" -> (tender \ "SYSTEM" \ "BUYER_GROUP_ID").text,
+      "buyerGroupName" -> (tender \ "SYSTEM" \ "BUYER_GROUP_NAME").text.trim,
+      "buyerOrgName" -> (tender \ "FD_CONTRACT" \ "CONTRACTING_AUTHORITY_INFORMATION" \ "NAME_ADDRESSES_CONTACT_CONTRACT" \ "CA_CE_CONCESSIONAIRE_PROFILE" \ "ORGANISATION").text.trim,
+      "buyerContact" -> (tender \ "FD_CONTRACT" \ "CONTRACTING_AUTHORITY_INFORMATION" \ "NAME_ADDRESSES_CONTACT_CONTRACT" \ "CA_CE_CONCESSIONAIRE_PROFILE" \ "E_MAIL").text.trim
     )
   }
 
