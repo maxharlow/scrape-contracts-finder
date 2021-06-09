bda1562b211d2d88203818ae921e7e12bd308526
Max Harlow <maxharlow@gmail.com>
2021-06-09 18:34:31 +0100

    Updates

--- README.md
+++ README.md
@@ -1,10 +1,8 @@
-Scrape Contracts
-================
+Scrape Contracts Finder
+=======================
 
-The [Crown Commercial Service](https://www.gov.uk/government/organisations/crown-commercial-service), the body responsible for awarding public sector contracts, publishes that data on [Contracts Finder](https://www.contractsfinder.service.gov.uk/).
+Some UK public sector contracts are published on [Contracts Finder](https://www.contractsfinder.service.gov.uk/).
 
-This uses [the Contracts Finder API](https://www.gov.uk/government/publications/open-contracting) to pull all the available records into a CSV file.
+This uses [the Contracts Finder API](https://www.contractsfinder.service.gov.uk/apidocumentation/V2) to pull all the available records into a CSV file.
 
-Requires [Node](https://nodejs.org/).
-
-Install the dependencies with `npm install`, then run `node contracts`. Produces a file named `contracts.csv`.
+Install the dependencies with `npm install`, then run `node contracts-finder`. Produces a file named `contracts-finder.csv`.
---
+++ contracts-finder.js
@@ -0,0 +1,95 @@
+import FSExtra from 'fs-extra'
+import Luxon from 'luxon'
+import Axios from 'axios'
+import AxiosRetry from 'axios-retry'
+import Ent from 'ent'
+import Scramjet from 'scramjet'
+
+async function fetch(location) {
+    const url = typeof location === 'object' ? location.url : location
+    const instance = Axios.create({ timeout: 30 * 1000 })
+    AxiosRetry(instance, {
+        retries: 10,
+        shouldResetTimeout: true,
+        retryCondition: e => {
+            return !e.response || e.response.status >= 500
+        },
+        retryDelay: (number, e) => {
+            if (number === 1) console.log(`Received code ${e.code}: ${url} (retrying...)`)
+            else console.log(`Received code ${e.code}: ${url} (retry ${number}...)`)
+            return 5 * 1000
+        }
+    })
+    const response = await instance(location)
+    return {
+        data: response.data
+    }
+}
+
+function dates(from, a = []) {
+    const date = Luxon.DateTime.fromFormat(from, 'yyyy-MM-dd')
+    if (date >= Luxon.DateTime.now()) return a
+    const datePlusOne = date.plus({ days: 1 }).toFormat('yyyy-MM-dd')
+    const request = {
+        method: 'POST',
+        url: 'https://www.contractsfinder.service.gov.uk/api/rest/2/search_notices/json',
+        headers: {
+            'content-type': 'application/json'
+        },
+        data: {
+            searchCriteria: {
+                statuses: 'Awarded',
+                publishedFrom: date.toFormat('yyyy-MM-dd'),
+                publishedTo: datePlusOne
+            },
+            size: 1000
+        }
+    }
+    return dates(datePlusOne, a.concat(request))
+}
+
+function detail(response) {
+    return response.data.noticeList.map(notice => {
+        return `https://www.contractsfinder.service.gov.uk/api/rest/2/get_published_notice/json/${notice.item.id}`
+    })
+}
+
+function awards(response) {
+    const notice = response.data.notice
+    return response.data.awards.map(award => {
+        return {
+            noticeURL: `https://www.contractsfinder.service.gov.uk/notice/${notice.id}`,
+            noticeFrom: Ent.decode(notice.organisationName),
+            noticeTitle: Ent.decode(notice.title),
+            noticeDescription: Ent.decode(notice.description).replace(/\r?\n+/g, ' ').replace(/ +/g, ' '),
+            noticeType: notice.type,
+            noticeStatus: notice.status,
+            noticeValueLow: notice.valueLow,
+            noticeValueHigh: notice.valueHigh,
+            noticeProcedureType: notice.procedureType,
+            noticeIsFrameworkAgreement: notice.isFrameworkAgreement,
+            noticeDeadlineDate: notice.deadlineDate,
+            noticePublishedDate: notice.publishedDate,
+            noticeCPVs: notice.cpvCodes.join('; '),
+            awardID: award.id,
+            awardValue: award.value,
+            awardSupplierValue: award.supplierAwardedValue, // what is this?
+            awardSupplierName: Ent.decode(award.supplierName),
+            awardSupplierCompanyDunsNumber: award.dunsNumber?.match(/^0+$/) ? null : award.dunsNumber,
+            awardSupplierAddress: award.supplierAddress.replace(/ ?\r?\n+/g, ', '),
+            awardProcedureType: award.awardedProcedureType
+        }
+    })
+}
+
+async function run() {
+    Scramjet.DataStream.from(dates('2016-01-01'))
+        .map(fetch)
+        .flatMap(detail)
+        .map(fetch)
+        .flatMap(awards)
+        .CSVStringify()
+        .pipe(FSExtra.createWriteStream('contracts-finder.csv'))
+}
+
+run()
